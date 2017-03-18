e55141f5dea6d49e763302abd9b492db5cc3faa1
Jun Ota <d14864@me.com>
2017-03-19 01:05:25 +0900

    5.6 complete

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,6 +1,5 @@
 
 arr = [42, 89, 23, 1]
-
 def search_array(a,i)
    counter = 0 
    r = a.each {|x| counter +=1 
@@ -17,10 +16,6 @@ end
 # p search_array(arr, 24)
 
 
-
-
-
- 
 def fib(x)
  counter =  2 
  fibonacci_array = [0,1]
@@ -34,3 +29,33 @@ end
  fib(6)
  fib(100)
  
+# Release 2
+# 1
+#  buble sort
+# 2
+#  web sites that has visuals of the concept really 
+#  helps the understanding.
+#  by searching many times,changing the search word helps.
+#  When i feel i need some sort of overall information,i go to 
+#  amazon and buy books.
+ 
+#  3. understanding the algorithm is interesting. to me it looks 
+#  like an explanation of a syntax. It is interesting to know how 
+#  the person or the method is defining each steps.
+
+#  4.
+  # input an array of integers 
+  # decide what criteria you want to sort(alphabetical,length,size)
+  # set a block of code to focus on every item in the array.
+  # define the method or action.
+  # print the result 
+
+# 5.
+  
+# ary = %w[jun miyu emily saki]
+# p ary
+
+# newary = ary.sort{|a,b| a <=> b}
+# p newary
+
+
