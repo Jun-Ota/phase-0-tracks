dd5774782f2265463bb2fa9573c6e31711101352
Jun Ota <d14864@me.com>
2017-03-21 18:41:53 +0900

    6.2 almost complete

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,69 @@
-class Puppy
+# class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
+	
+#   def speak(number)
+# 	number.times do puts "Woof!"
+# 	end
+#   end 
+
+#   def roll_over
+# 	puts "*rolls over*"
+#   end
+
+#   def dog_years(human_age)
+#     dog_year = human_age * 6 
+#     puts dog_year
+#   end
+
+#   def bark
+# 	puts "bark"
+#   end
+
+#   def initialize
+#   	 puts "Initializing new puppy instance ..."
+#   end 
+
+# end
+
+# pupy = Puppy.new()
+# pupy.fetch("ball")
+# pupy.speak(8)
+# pupy.roll_over
+# pupy.dog_years(35)
+# pupy.bark
+
+class  Golf 
+  def initialize 
+   puts " Playing golf is wonderful!!"
+  end 
+
+  def club_selecter(yards_left)
+   if yards_left > 150
+   	 puts "3w"
+   elsif 100 < yards_left < 150
+     puts "7I"
+   else
+     puts "P"
+   end
+  end
+
+  def putt_counter(number)
+  	puts "I cannnot believe you had #{number} times of 3 putt!"
   end
 
 end
+
+golf = Golf.new
+golf.club_selecter(180)
+golf.putt_counter(4)
+
+
+
+
+
+
 
