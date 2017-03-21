9ced082662ce9b3746d1a521f223174547a98525
Jun Ota <d14864@me.com>
2017-03-21 23:55:05 +0900

    release 1 done

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,12 +7,34 @@ class Santa
    puts "That was a good #{cookie_type}!" 
   end
 
-  def initialize
-   puts "Initializing Santa instance ..."
-  end
-
+  def initialize(gender,ethnicity)
+   # puts "Initializing Santa instance ..."
+   @gender = gender 
+   @ethnicity = ethnicity
+   # @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+   @age = 0 
+   end
 end
 
-santa = Santa.new 
-santa.speak
-santa.eat_milk_and_cookies("snickerdoodle")
+# santa = Santa.new()
+# santa.speak
+# santa.eat_milk_and_cookies("snickerdoodle")
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+# p santas
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p santas
