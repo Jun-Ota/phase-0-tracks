8a7137f1700d0077002df9d688ad750152389d81
Jun Ota <d14864@me.com>
2017-03-21 17:25:27 +0900

    6.1 finish

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+SPECIES ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Friendly: yes
+Eye count: 2
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Jump
+Hug
+run
