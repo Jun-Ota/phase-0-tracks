15a5d4c27128e74c2635c99173980af3c704c436
Jun Ota <d14864@me.com>
2017-03-23 11:30:18 +0900

    work at office 0323

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,6 @@
 class Santa
+  attr_reader :gender, :ethnicity
+  attr_accessor :country, :height
   def speak
    puts "Ho, ho, ho! Haaaappy holidays!" 
   end
@@ -8,40 +10,62 @@ class Santa
   end
 
   def initialize(gender,ethnicity,country,height)
-   # puts "Initializing Santa instance ..."
-   @gender = gender 
-   @ethnicity = ethnicity
-   @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-   @country = country
-   @height = height
-   @age = 0 
+    # puts "Initializing Santa instance ..."
+	   @gender = gender 
+	   @ethnicity = ethnicity
+	   @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	   @country = country
+	   @height = height
+	   @age = 0 
    end
-
    def celebrate_birthday 
      age = @age + 1 
    end
-
    def get_mad_at(reindeer_name)
-     reindeer_name.index.last 
+     @reindeer_ranking  << reindeer_name 
+     p @reindeer_ranking
    end 
+     # getter method(readable)
+   # def age 
+   # 	@age
+   # end
 
-   def gender = (new_gender)
-   	@gender = new_gender
-   end 
-   # getter method
-   def age 
-   	@age
-   end
-   def ethnicity 
-   	@ethnicity
-   end
+   # def ethnicity 
+   # 	@ethnicity
+   # end
+
+   # def gender
+   #  @gender 
+   # end 
+
+   # def country
+   # 	@country
+   # end
+
+   # def height 
+   # 	@height
+   # end
+
+   #setter (readable,editable)
+    # def gender=(new_gender)
+    #  @gender = new_gender 
+    # end
 
 end
 
-# Driver code 1
-# santa = Santa.new()
-# santa.speak
-# santa.eat_milk_and_cookies("snickerdoodle")
+# Driver code 4
+santa = Santa.new("agender", "black","japan","180")
+# santa.get_mad_at("Jacob")
+# santa.gender = "something new"
+puts "The santa is a #{santa.gender}"
+
+# Driver code 3
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+# p santas
 
 # Driver code 2
 # santas = []
@@ -54,13 +78,10 @@ end
 # santas << Santa.new("N/A", "N/A")
 # p santas
 
-# Driver code 3
-# santas = []
-# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-# example_genders.length.times do |i|
-#   santas << Santa.new(example_genders[i], example_ethnicities[i])
-# end
-# p santas
+# Driver code 1
+# santa = Santa.new()
+# santa.speak
+# santa.eat_milk_and_cookies("snickerdoodle")
 
-# Driver code 4
+
+
