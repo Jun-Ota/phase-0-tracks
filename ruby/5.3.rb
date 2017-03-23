6aeb1bec2f43d671dbc8a56d5e99f5a4a0fbe6ab
Jun Ota <d14864@me.com>
2017-03-23 22:06:10 +0900

    small change

--- ruby/5.3.rb
+++ ruby/5.3.rb
@@ -12,7 +12,7 @@
 #   Pw:"120y",
 #   Sw:"90y"
 # }
-# Why is the hash above error???
+ Question1:Why is the hash above error???
 
 
 golf_course = ["ABC","Phoenix","Gotenba","Yomiuri","Naruo","Nishinomiya-kogen"]
@@ -29,12 +29,13 @@ golf_course = ["ABC","Phoenix","Gotenba","Yomiuri","Naruo","Nishinomiya-kogen"]
 # A method that will remove items from a data structure until the condition
 # in the block evaluates to false, then stops 
 # (you may not find a perfectly working option for the hash, and that's okay).
-   golf_course.delete_if { |course|
-           bool= false
-           break if course == "Yomiuri"
-           
-    }
-    p golf_course
+   # golf_course.delete_if { |course|
+   #         bool= false
+   #         break if course == "Yomiuri"
+   #         bool =true
+   #  }
+   #  p golf_course
+    Question2 :Why do I need to add "bool = true "?
 
 
 # Driver 3 
@@ -49,14 +50,12 @@ golf_course = ["ABC","Phoenix","Gotenba","Yomiuri","Naruo","Nishinomiya-kogen"]
 #  golf_course.keep_if {|course| course == "ABC"}
 #  p golf_course
 # }
-# How do you set two condition in? ABC and Gotenba.
+ Question 3: How do you set two condition in? ex ABC and Gotenba.
 
 # golf_club.each {|club, yard|
 #  golf_club.keep_if {|club,yard| yard == "230y" }
 #  p golf_club
 # }
-
-
 
 # Driver 1
 # golf_course.each {|course|
@@ -64,7 +63,7 @@ golf_course = ["ABC","Phoenix","Gotenba","Yomiuri","Naruo","Nishinomiya-kogen"]
 #  p golf_course
 # }
 
-# Question Why does this print 5 times????
+ Question 4 : Why does this print 5 times????
 
 # golf_club.each {|club, yard| 
 #  golf_club.delete_if {|club, yard| club != "1w" }
@@ -90,7 +89,3 @@ golf_course = ["ABC","Phoenix","Gotenba","Yomiuri","Naruo","Nishinomiya-kogen"]
 # golf_club.map do |club,yard|
 #  puts "#{club}"
 # end
-
-
-
-
