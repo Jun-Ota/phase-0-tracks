7735829e525ed956493426414c018e21af131918
Jun Ota <d14864@me.com>
2017-03-24 11:53:08 +0900

    Friday morning session

--- ruby/4.1.practice.rb
+++ ruby/4.1.practice.rb
@@ -1,18 +1,114 @@
 
+=====================================
+# Assignment 3: The Mega-Complimentizer
+# Ask the user if they're in a good mood.
+# If they aren't, keep telling them
+# that they're fabulous, and then asking
+# again if they're in a good mood.
 
+mood = false
+until mood 
+ puts "Are you in a good mood?(y/n)"
+  mood = gets.chomp
+  if mood == "n"
+    puts "You are fabulous!"
+    mood = false
+  elsif mood == "y" 
+    puts "I knew I could cheer you up!"
+    mood = true
+  else 
+    mood = false
+  end
+end
+# When the user finally answers "y", say "I knew I could cheer you up!"
+=====================================
+# NUMBER CRUNCHER 
+# Write a method that takes a number as an argument. If the number is greater than 20
+# count down from the number by 2's
+# If the number is less than 20 count down from the number by 1's
+# Display the numbers as they count down to 0.
+
+def number(x)
+ until x == 0 
+  if x > 20
+     puts x 
+     x -= 2 
+  else x < 20
+     puts x 
+     x -= 1
+  end
+ end 
+end 
+
+number(30)
+number(4)
+=====================================
+# 1. Write a method that adds two strings together.
+def adder(msg,str)
+    msg + str
+end 
+puts adder("wide","range")
+# 2. Count up to 20, printing each number and whether it is even or odd.
+
+def count_up(n)
+   current_count = 0 
+   while current_count < 20 
+   if current_count.even?
+     puts "#{current_count} even"
+   elsif  current_count.odd?
+      puts "#{current_count} odd"
+   end
+    current_count += 1 
+   end
+end
+
+count_up(0)
+
+# 3. Write a method that asks the user for their age, then says their age
+# ain't nothing but a number. example: User enters "46"
+#    output: "46 ain't nothing but a number!"
+def odd_number_detector(n)
+   current_count = 3
+    
+ while current_count < n
+  if current_count.odd?
+   puts "#{current_count}!"
+  end
+  current_count += 3
+ end
+ 
+end
+
+odd_number_detector(36)
+=====================================
+# Assignment 2: Global Greetings
+# Ask the user how they would like to be greeted.
+
+puts "In what language do you want to be greeted?(French/Spanish/Afrikaans)"
+language = gets.chomp
+if language == "French"
+   puts "Bonjour!"
+elsif language == "Spanish"
+  puts "Hola!"
+elsif language == "Afrikaans"
+ puts "Hallo!"
+else 
+  puts "Uh,hi?"
+end 
+
+# If they say "in French", respond with "Bonjour!"
+# If they say "in Spanish", respond with "Hola!"
+# If they say "in Afrikaans", respond with "Hallo!"
+# Handle all other input with "Uh, hi?"
 =====================================
 def multiply_add(x,y,z)   
     x * y + z
 end
 puts multiply_add(1,3,4)
 
-# mult_then_add(1, 3, 4)
-# ... would return 7
+# mult_then_add(1, 3, 4) ... would return 7
 
-
-# 2.
-# Write a method that reverses
-# a string ONLY if it's shorter than
+# 2. Write a method that reverses a string ONLY if it's shorter than
 # 4 letters. 
 
 def reverse_under4(msg)
@@ -26,17 +122,12 @@ end
 puts reverse_under4("yo")
 puts reverse_under4("hello")
 
-
 # Otherwise, the string is returned as-is.
-
 # (Hint: strings have a built-in .length method!)
-
 # conditional_reverse("yo")
 # => "oy"
-
 # conditional_reverse("hello")
 # => "hello"
-
 # 3. What does the built-in .chop
 # method on a string do?
 # What are some ways to find out?
