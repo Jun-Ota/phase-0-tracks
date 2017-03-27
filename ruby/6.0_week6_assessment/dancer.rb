091e1e4f11df6b6cabbe81604ba35ad60122d9e3
Jun Ota <d14864@me.com>
2017-03-27 14:52:41 +0900

    WIP

--- ruby/6.0_week6_assessment/dancer.rb
+++ ruby/6.0_week6_assessment/dancer.rb
@@ -6,28 +6,52 @@ class Dancer
  def initialize(name,age) 
  	@name = name
  	@age  = age
+ 	@list = []
  end
 
  def name
    @name
  end
-
+ 
  def age
    @age
  end
-
+ 
  def write(age)
   @age = age 
  end
-
+ 
  def pirouette
   "*twirls*"
  end
-
-
+ 
  def bow
   "*bows*"
  end
+ 
 
+ def card 
+ 	
+ 	def queue_dance_with(dancer1)
+     @list << dancer1
+    end 
+    
+    @list
+
+ end
+
+ def card 
+   # @card << @waiting
+ end
 
 end
+
+
+
+
+
+
+
+
+
+
