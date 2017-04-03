c9857ad84abaf8e3b0142ea93974b4dfaeb62b39
Jun Ota <d14864@me.com>
2017-04-03 18:50:37 +0900

    7.3 complete

--- js/algos.js
+++ js/algos.js
@@ -1,16 +1,12 @@
-//==================Release 1====================
+//==================Release 0====================
 // RELEASE1: Pseudcode
-// take an array as an input
+// input: array 
 // output: the longest phrase.
-// need to compare the lenght."some string".length
-// iterate through the array to change string into integer
-// which shows the length.
-// chose the biggest number.
-// How do we do that??
-// can we sort?? 
+// 1.build a loop to go through the array
+// 2.change the string into number(.length)
+// 3.chose the biggest number.
 var  phrase = ["long phrase","longest phrase","longer phrase"]
-var new_phrase = []
- 
+var new_phrase = [] 
 function max(array){
  for (var i=0; i < phrase.length; i++){
    new_phrase.push(phrase[i].length);
@@ -19,50 +15,74 @@ function max(array){
  }
 }
 console.log(max(phrase));
-
+// Reference code
 // var a = [3, 1, 4, 1, 5, 9, 2, 6];
 // Math.max.apply(null, a);  // → 9
+//==================Release 1====================
+// Pesudocode 
+// input : key-value pair
+// return: if something in common => true
+//         else  => false
+// 1.build function that takes two hashes.
+// 2.call the key and compare.
+// 3.call the value and compare.
+// 4.set if else statment to return.
+// 5.  2 || 3 => true. else => false
+
+function release1(hash1,hash2){
+  if (hash1.key==hash2.key || hash1.value==hash2.value) {
+    console.log("true");
+  } else {
+     console.log("false");
+  }
+}
+hash1 =  {name: "Steven", age: 54} 
+hash2 =  {name: "Tamir", age: 54}
+release1(hash1,hash2)
+
 //==================Release 2==================== 
-function release2 (){
-  result_array = [];
-    var l = 10;
-    var c = " abcdefghijklmnopqrstuvwxyz";
-    var cl = c.length;
-    var r = "";
-    
-  for(var i=0; i< l; i++){
-    r += c[Math.floor(Math.random()*cl)];
-    // console.log(r);
-  }
-  result_array.push(r);
-  console.log(result_array);
-}
-
-function array_maker(integer){
-   final_array =[];
-  for (var i = 0 ; i < integer ; i++) {
-    final_array.push(release2());
-  // console.log(final_array)
-  }
-  console.log(final_array)
-}
-
- function release2_2(){
-  for (var i=0 ; i < 11; i++){
-  	max(array_maker());
-  }
- }
-
 // Pseudocode
 // 1. Build a function that returns random string with random length. 
 // 1-1. put the result into an array.
-// 2. Build a method that do method 1 for n times(n = input)
+//   => method release2
+// 2. Build a method that do "release2" for n times(n = input)
 //  ==>estimated resutl  
 //  if length = 3  final_array = ["afsdfd","fedhsa","iofasfdsa"]
+//  ==> method array_maker
 
 // 3.Build a method that does the below
 //  =>method to return an array with random length.
 //  =>apply the return into method built in release 1.
+//  => method release2_2
+
+function release2(){
+  first_array = [];
+    var l = 11;
+    var c = " abcdefghijklmnopqrstuvwxyz";
+    var cl = c.length;
+    var r = "";
+
+  for(var i=0; i< l; i++){
+    r += c[Math.floor(Math.random()*cl)];
+    // returning random letter or space into r.until 0~10
+    console.log(r);
+    // check the letter.　
+  }
+   first_array.push(r);
+   console.log(first_array);
+}
+release2()
+
+function array_maker(integer){
+  final_array =[];
+  for (var i = 0 ; i < integer ; i++) {
+    final_array.push(release2());
+  console.log(final_array)
+  }
+  console.log(final_array)
+}
+array_maker(2)
+
 
 // =====================Questions===================
 // 1: How to do random length? (I added space in the candidate.)
